2b2d93b589d95f7e870fa3d9be354cf693c26acd
vinay <vinaychakravarthi10110@gmail.com>
2024-04-28 16:17:43 +0530

    extended theme resolved

--- client/src/expanded-theme.tsx
+++ client/src/expanded-theme.tsx
@@ -1,10 +1,9 @@
-// eslint-disable-next-line
-import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import  "@mui/material/styles/createPalette";
 declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
     [key: number]: string;
   }
-
   interface Palette {
     tertiary: PaletteColor;
   }
